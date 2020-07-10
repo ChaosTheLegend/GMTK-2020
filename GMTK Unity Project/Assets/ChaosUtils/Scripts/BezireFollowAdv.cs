b7b3a381c8801a0b3fd1539a2753b79cf56a322a
ChaosTheLegend <chaosthelegend@inbox.ru>
2020-07-10 21:53:56 +0600

    Created Unity Project
    Created Project
    imported TMPro
    imported PixelPerfect Camera
    imported ChaosUtils (Custom Package with commonly used scripts)

---
+++ GMTK Unity Project/Assets/ChaosUtils/Scripts/BezireFollowAdv.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezireFollowAdv : MonoBehaviour
+{
+    public BezierCurvePathAdv route;
+    public float Speed = 1f;
+
+    private Vector2 position;
+
+    public bool folowing = false;
+
+    public enum EndActions {Stop = 0,Restart=1,GoBack=2}
+    public EndActions onEnd;
+    [HideInInspector]
+    public float tParam;
+    [HideInInspector]
+    public int part;
+    private bool allowCorutine;
+
+    //[HideInInspector]
+    // Start is called before the first frame update
+    void Start()
+    {
+        tParam = 0f;
+        allowCorutine = true;
+        part = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowCorutine && folowing)
+        {
+            if (Speed > 0)
+            {
+                part = 0;
+                tParam = 0f;
+                StartCoroutine(FollowPathForward(onEnd));
+            }
+            if (Speed < 0)
+            {
+                part = (route.controllPoints.Count / 3)-1;
+                tParam = 1f;
+                StartCoroutine(FollowPathBackward(onEnd));
+            }
+        }
+    }
+    private IEnumerator FollowPathBackward(EndActions onEnd) {
+        allowCorutine = false;
+        while (part >= 0)
+        {
+            Vector2 p1 = route.controllPoints[part * 3];
+            Vector2 p2 = route.controllPoints[part * 3 + 1];
+            Vector2 p3 = route.controllPoints[part * 3 + 2];
+            Vector2 p4 = route.controllPoints[part * 3 + 3];
+            while (tParam > 0)
+            {
+                tParam += Time.deltaTime * Speed * (route.controllPoints.Count / 3);
+                position = Mathf.Pow(1 - tParam, 3) * p1 +
+                    3 * Mathf.Pow(1 - tParam, 2) * tParam * p2 +
+                    3 * Mathf.Pow(tParam, 2) * (1 - tParam) * p3 +
+                    Mathf.Pow(tParam, 3) * p4;
+                transform.position = position;
+                yield return new WaitForEndOfFrame();
+            }
+            tParam = 1f;
+            part--;
+            yield return new WaitForEndOfFrame();
+        }
+        allowCorutine = true;
+        if ((int)onEnd == 2) {
+            folowing = true;
+            Speed = -Speed;
+        }
+        if ((int)onEnd == 1)
+        {
+            folowing = true;
+        }
+        if ((int)onEnd == 0) folowing = false;
+    }
+    private IEnumerator FollowPathForward(EndActions onEnd) {
+        allowCorutine = false;
+        while (part < (route.controllPoints.Count / 3))
+        {
+            Vector2 p1 = route.controllPoints[part * 3];
+            Vector2 p2 = route.controllPoints[part * 3 + 1];
+            Vector2 p3 = route.controllPoints[part * 3 + 2];
+            Vector2 p4 = route.controllPoints[part * 3 + 3];
+            while (tParam < 1)
+            {
+                tParam += Time.deltaTime * Speed * (route.controllPoints.Count / 3);
+                position = Mathf.Pow(1 - tParam, 3) * p1 +
+                    3 * Mathf.Pow(1 - tParam, 2) * tParam * p2 +
+                    3 * Mathf.Pow(tParam, 2) * (1 - tParam) * p3 +
+                    Mathf.Pow(tParam, 3) * p4;
+                transform.position = position;
+                yield return new WaitForEndOfFrame();
+            }
+            tParam = 0f;
+            part++;
+            yield return new WaitForEndOfFrame();
+        }
+        tParam = 0f;
+        allowCorutine = true;
+        if ((int)onEnd == 2)
+        {
+            folowing = true;
+            Speed = -Speed;
+        }
+        if ((int)onEnd == 1)
+        {
+            folowing = true;
+            part = 0;
+        }
+        if ((int)onEnd == 0) folowing = false;
+    }
+
+}
+
---
+++ GMTK Unity Project/Assets/ChaosUtils/Scripts/RadialLayoutGroup.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RadialLayoutGroup : MonoBehaviour
+{
+    [Header("Manual Distribution")]
+    public float initAngle;
+    public float Spacing;
+    public float centerDistance;
+    [Header("Auto Distribution")]
+    public bool autoDistribute;
+    public float endAngle;
+    [Header("Children")]
+    public bool rotateChildren;
+    public float childAngle;
+    [Header("Auto Size")]
+    public bool autoSize;
+    public float childWidth;
+    public float childHeight;
+
+    public int unlocked = 1;
+    public RectTransform rt;
+    public RectTransform[] Children;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        rt = GetComponent<RectTransform>();
+        LayoutUpdate();
+        //Children = rt.GetComponentsInChildren<RectTransform>();
+        
+    }
+
+    private void LayoutUpdate()
+    {
+        Children = new RectTransform[transform.childCount];
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            Children[i] = child.GetComponent<RectTransform>();
+            i++;
+        }
+    }
+    public void Unlock()
+    {
+        unlocked += 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 center = rt.position;
+        if (autoDistribute)
+        {
+            Spacing = (endAngle - initAngle) / (unlocked + 1);    
+        }
+        for (int i = 1; i <= unlocked; i++)
+        {
+            Children[i-1].gameObject.SetActive(true);
+            float childx = centerDistance * Mathf.Cos((initAngle + Spacing * i) * Mathf.Deg2Rad);
+            float childy = centerDistance * Mathf.Sin((initAngle + Spacing * i) * Mathf.Deg2Rad);
+            Vector2 childpos = center + new Vector2(childx, childy);
+            Children[i-1].position = childpos;
+            if (rotateChildren)
+            {
+                Children[i-1].rotation = Quaternion.Euler(0f, 0f, initAngle + Spacing * i + childAngle - 90);
+            }
+            if (autoSize)
+            {
+                //Children[i-1].sizeDelta = new Vector2(childWidth, childHeight);
+                Children[i - 1].transform.localScale = new Vector2(childWidth, childHeight);
+            }
+        }
+        for (int i = unlocked; i < Children.Length; i++)
+        {
+            Children[i].gameObject.SetActive(false);
+        }
+    }
+}
