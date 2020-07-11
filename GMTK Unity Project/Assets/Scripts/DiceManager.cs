75bc146efbebf396d8dda28af2df8293e05e29d0
ChaosTheLegend <chaosthelegend@inbox.ru>
2020-07-11 17:19:58 +0600

    Added Game Build

---
+++ GMTK Unity Project/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    [SerializeField] private List<DieSide> outcomes;
+    [SerializeField] private int RepeatCooldown;
+    private List<DieSide> history;
+    
+    public void Roll()
+    {
+        DieSide roll = PseudoRandom(outcomes, history, RepeatCooldown);
+        history.Add(roll);
+
+        if(history.Count > RepeatCooldown + 2)
+        {
+            history.RemoveAt(0);
+        }
+    }
+
+    private DieSide PseudoRandom(List<DieSide> choises, List<DieSide> history, int RepeatCooldown)
+    {
+        List<DieSide> resticted = history.GetRange(history.Count - RepeatCooldown - 1, RepeatCooldown);
+        foreach (DieSide res in resticted)
+        {
+            if (choises.Contains(res))
+            {
+                choises.Remove(res);
+            }
+        }
+
+        int maxrange = choises.Count;
+        int rand = Random.Range(0, maxrange);
+        return (choises[rand]);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ GMTK Unity Project/Assets/Scripts/DieSide.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Side",menuName = "Die Side")]
+public class DieSide : ScriptableObject
+{
+    public Sprite SideSprite;
+    public int id;
+    public string Name;
+    [TextArea]
+    public string Description;
+}
